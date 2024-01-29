95f7ea10b42bdf58abf7d88cc039a2da40c48ecb
Marcos Winicios Pereira Martins <pmarcoswinicios@gmail.com>
2024-01-29 03:39:44 -0300

    feat: Adding configuration Class for access permissions

---
+++ src/main/java/com/studies/gamelist/config/SecurityConfigurations.java
@@ -0,0 +1,66 @@
+package com.studies.gamelist.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.studies.gamelist.security.JwtFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfigurations {
+
+//	@Bean
+//	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//		http.csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).disable())
+//			.authorizeHttpRequests((authorize) -> {
+//				authorize.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
+//						.requestMatchers(HttpMethod.POST, "/auth/login")
+//						.permitAll().requestMatchers(HttpMethod.POST, "/user").permitAll()
+//						.requestMatchers(HttpMethod.GET, "/user").hasRole("ADMIN")
+//						.requestMatchers(HttpMethod.GET, "/lists").hasRole("ADMIN")
+//						.requestMatchers(HttpMethod.GET, "/game").hasRole("DEFAULT")
+//						.anyRequest().authenticated();
+//			})
+//			.headers(headers -> headers.frameOptions().disable())
+//			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//
+//		http.addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
+//
+//		return http.build();
+//	}
+	
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		
+		/*Configuração de permissão para o h2 database*/
+		http.csrf(csrf -> csrf.ignoringRequestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")))
+		.authorizeHttpRequests(auth -> auth.requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll())
+		.headers(headers -> headers.frameOptions().disable());
+
+		http.csrf(csrf -> csrf.disable())
+			.authorizeHttpRequests((authorize) -> {
+				authorize.requestMatchers(HttpMethod.POST, "/auth/login")
+						.permitAll().requestMatchers(HttpMethod.POST, "/user").permitAll()
+						.requestMatchers(HttpMethod.GET, "/user").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.GET, "/lists").hasRole("ADMIN")
+						.requestMatchers(HttpMethod.GET, "/game").hasRole("DEFAULT")
+						.anyRequest().authenticated();
+			}).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		
+		
+
+		http.addFilterBefore(new JwtFilter(), UsernamePasswordAuthenticationFilter.class);
+
+		return http.build();
+	}
+
+}
